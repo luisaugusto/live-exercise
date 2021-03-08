5285d1164abd61d976caaa266c9a98877f7fd54a
Luis Augusto <hello@luis.app>
2021-03-07 19:43:04 -0800

    Deleted app.js file so it can be recreated as part of exercise

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { shallow } from 'enzyme';
+import AppClassComponent from "./AppClassComponent";
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const component = shallow(<AppClassComponent />);
+  expect(component).toHaveLength(1)
 });
