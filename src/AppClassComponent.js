db9b46a9b537af3fae8762718e549810ca98b198
Luis Augusto <hello@luis.app>
2021-03-07 19:39:23 -0800

    Setup index file with class component

---
+++ src/AppClassComponent.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            index: 1,
+            image: null,
+            allowUpdates: true
+        }
+    }
+
+    fetchImage() {
+        const url = `https://jsonplaceholder.typicode.com/photos/${this.state.index}`
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                fetch(url)
+                    .then(resp => resp.json())
+                    .then(resp => resolve(resp))
+            }, 200)
+        })
+    }
+
+    componentDidMount() {
+        this.fetchImage().then(res => {
+            this.state.allowUpdates && this.setState(() => ({image: res}))
+        });
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.state.index === prevState.index) return;
+        this.fetchImage().then(res => {
+            this.state.allowUpdates && this.setState(() => ({image: res}))
+        });
+    }
+
+    componentWillUnmount() {
+        this.setState(() => ({
+            allowUpdates: false
+        }))
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <button
+                    className="btn" type="button"
+                    onClick={() => this.setState(state => ({index: state.index - 1}))}>
+                    Prev
+                </button>
+                <div className="image-holder">
+                    { this.state.image && <img src={this.state.image.url} /> }
+                </div>
+                <button
+                    className="btn"
+                    type="button"
+                    onClick={() => this.setState(state => ({ index: state.index + 1 }))}>
+                    Next
+                </button>
+            </div>
+        )
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AppClassComponent from "./AppClassComponent";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppClassComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
